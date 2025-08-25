7682fcea50468d74872b1d82295c45c58d4438a2
Kofi Otuo <otuokofi@outlook.com>
2025-08-25 21:46:59 +0000

    update

--- packages/apps/src/Menu/index.tsx
+++ packages/apps/src/Menu/index.tsx
@@ -27,17 +27,11 @@ interface Props {
 function createExternals (t: (key: string, optionsOrText?: string | { replace: Record<string, unknown> }, options?: { ns: string }) => string): ItemRoute[] {
   return [
     {
-      href: 'https://github.com/polkadot-js/apps',
+      href: 'https://github.com/XORIONCHAIN',
       icon: 'code-branch',
       name: 'github',
       text: t('nav.github', 'GitHub', { ns: 'apps-routing' })
     },
-    {
-      href: 'https://wiki.polkadot.network',
-      icon: 'book',
-      name: 'wiki',
-      text: t('nav.wiki', 'Wiki', { ns: 'apps-routing' })
-    }
   ];
 }
 
